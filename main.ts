e2f4783218796173aaa6d18b8c295fda5008df49
emilytwines <60051909+emilytwines@users.noreply.github.com>
2022-08-22 12:36:27 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -24,9 +24,6 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
 })
 let enemies: Sprite = null
 let executed = false
-let level3 = 0
-let level2 = 0
-let level1 = 0
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -56,8 +53,6 @@ let gameLength = 60
 let level3Time = 40
 let level2Time = 20
 let enemySpeed = 25
-let levels = [level1, level2, level3]
-let currentLevel = level1
 let splashed_1 = false
 let splashed_2 = false
 let splashed_3 = false
@@ -68,19 +63,16 @@ let gameStart = true
 game.splash("Have a Heart")
 game.onUpdateInterval(1000, function () {
     if (game.runtime() / 1000 >= level3Time && splashed_3 == false) {
-        currentLevel = level3
         picture.replace(4, 1)
         game.splash("Level 3")
         enemySpeed += 30
         splashed_3 = true
     } else if (game.runtime() / 1000 >= level2Time && splashed_2 == false) {
-        currentLevel = level2
         picture.replace(3, 4)
         game.splash("Level 2")
         enemySpeed += 30
         splashed_2 = true
     } else if (game.runtime() / 1000 < level2Time && splashed_1 == false) {
-        currentLevel = level1
         game.splash("Level 1")
         splashed_1 = true
     }
